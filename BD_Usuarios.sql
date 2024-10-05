c036c3b48bd53b77084ebc6773186b7f77ab5849
Uriel <luevanosaavedra@gmail.com>
2024-10-05 10:00:02 -0600

    Se creo el CRUD de la tabla de usuarios

--- BD_Usuarios.sql
+++ BD_Usuarios.sql
@@ -26,3 +26,51 @@ CREATE table taller
     marca varchar(255),
     descripcion varchar(255)
 );
+
+-- Procedimiento almacenado para insertar usuarios --
+CREATE procedure p_insertar_usuarios
+(
+    in _nombre varchar(255),
+    in _apellidop varchar(255),
+    in _apellidom varchar(255), 
+    in _fecha_nacimiento DATE,
+    in _rfc VARCHAR(13) 
+)
+begin 
+    insert into usuarios (nombre, apellidop, apellidom, fecha_nacimiento, rfc) values 
+    (_nombre, _apellidop, _apellidom, _fecha_nacimiento, _rfc);
+end;
+
+call p_insertar_usuarios('Jose', 'Perez', 'Prado', '2000-06-11', 'GGTN260803EM5')
+select * from usuarios; 
+
+-- Procedimiento almacenado para modificar usuarios --
+create procedure p_modificar_usuarios
+(
+    in _id_usuario int, 
+    in _nombre varchar(255),
+    in _apellidop varchar(255),
+    in _apellidom varchar(255), 
+    in _fecha_nacimiento DATE,
+    in _rfc VARCHAR(13) 
+)
+begin
+    update usuarios set nombre = _nombre, apellidop = _apellidop, apellidom = _apellidom, fecha_nacimiento = _fecha_nacimiento, rfc = _rfc
+    where id_usuario = _id_usuario;
+end; 
+
+call p_modificar_usuarios(1, 'Manuel', 'Perez', 'Prado', '2000-06-11', 'GGTN260803EM5'); 
+select * from usuarios; 
+
+-- Procedimiento almacenado para eliminar usuarios --
+CREATE procedure p_eliminar_usuarios
+(
+    in _id_usuario int
+)
+begin
+    delete from usuarios
+    where id_usuario = _id_usuario; 
+end; 
+
+call p_eliminar_usuarios(1); 
+select * from usuarios; 
