c1fd7edff59653ba220a93be9ff73ba53b0b7622
Uriel <luevanosaavedra@gmail.com>
2024-10-05 19:54:24 -0600

    Se agrego la tabla de permisos y se hicieron modificaciones, a permisos y usuarios

--- BD_Usuarios.sql
+++ BD_Usuarios.sql
@@ -9,6 +9,52 @@ create table usuarios
     rfc VARCHAR(13)
 );
 
+-- Modificacion a la tabla de usuarios --
+alter table usuarios
+add unique (rfc); 
+
+-- Verificar que se haya hecho el cambio en la tabla usuarios --
+describe usuarios; 
+
+-- Tabla de permisos --
+create table permisos
+(
+    id_permiso int PRIMARY key auto_increment,
+    tipo_permiso varchar(100),
+    fk_id_usuario int, 
+    foreign key (fk_id_usuario) references usuarios (id_usuario)
+); 
+
+-- Modificaciones a la tabla de permisos --
+ALTER table permisos
+add column user varchar(100);
+
+alter table permisos 
+add column pass varchar(20);
+
+ALTER table permisos
+modify column pass varchar(255); 
+
+ALTER TABLE permisos DROP FOREIGN KEY permisos_ibfk_1;
+
+alter table permisos
+drop column fk_id_usuario;
+
+alter table permisos 
+add column fk_rfc varchar(13); 
+
+ALTER table permisos 
+add foreign key (fk_rfc) references usuarios (rfc); 
+
+-- Verificar que las modificaciones si se hicieron --
+describe permisos; 
+
+/* Hacemos un insercion de datos para poder usar las dos tablas para 
+ver el funcionamientod*/
+call p_insertar_usuarios('Jose', 'Perez', 'Prado', '2000-06-11', 'GGTN260803EM5');  
+insert into permisos values (null, 'Administrador', 'jose11', sha1('1234'), 'GGTN260803EM5');
+SELECT * from permisos; 
+
 -- Tabla de refacciones --
 create table refacciones
 (
@@ -41,7 +87,7 @@ begin
     (_nombre, _apellidop, _apellidom, _fecha_nacimiento, _rfc);
 end;
 
-call p_insertar_usuarios('Jose', 'Perez', 'Prado', '2000-06-11', 'GGTN260803EM5')
+call p_insertar_usuarios('Jose', 'Perez', 'Prado', '2000-06-11', 'GGTN260803EM5');  
 select * from usuarios; 
 
 -- Procedimiento almacenado para modificar usuarios --
@@ -73,4 +119,4 @@ begin
 end; 
 
 call p_eliminar_usuarios(1); 
-select * from usuarios; 
+select * from usuarios;
