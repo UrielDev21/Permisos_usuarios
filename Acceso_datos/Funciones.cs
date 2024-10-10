e6199b6b5f9809deb377de7d309bf344eb887806
Uriel <luevanosaavedra@gmail.com>
2024-10-09 19:49:52 -0600

    Ya funciona el formulario para insertar herramientas

--- Acceso_datos/Funciones.cs
+++ Acceso_datos/Funciones.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Conexion;
+
 namespace Acceso_datos
 {
     public class Funciones
--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -16,7 +16,7 @@ namespace Usuarios
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AgregarUsuarios());
+            Application.Run(new AgregarHerramientas());
         }
     }
 }
