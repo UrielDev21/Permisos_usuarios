58cf2750a455c865b21fb15cf5740bdc5901a890
Uriel <luevanosaavedra@gmail.com>
2024-10-10 07:43:48 -0600

    Se agrego un nuevo boton

--- Usuarios/Menu.Designer.cs
+++ Usuarios/Menu.Designer.cs
@@ -28,155 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.optAdministrar_usuarios = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.optAgregar_Herramientas = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.optAgregar_refacciones = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.optBuscar_herramientas = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.optBuscar_refacciones = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel5 = new System.Windows.Forms.ToolStripLabel();
+            this.optBuscar_usuarios = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel6 = new System.Windows.Forms.ToolStripLabel();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
-            this.toolStrip1.SuspendLayout();
+            this.optSalir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel7 = new System.Windows.Forms.ToolStripLabel();
+            this.toolstrip = new System.Windows.Forms.ToolStrip();
+            this.toolstrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // toolStrip1
+            // optAdministrar_usuarios
             // 
-            this.toolStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripLabel1,
-            this.toolStripButton2,
-            this.toolStripLabel2,
-            this.toolStripButton3,
-            this.toolStripLabel3,
-            this.toolStripButton4,
-            this.toolStripLabel4,
-            this.toolStripButton5,
-            this.toolStripLabel5,
-            this.toolStripButton6,
-            this.toolStripLabel6});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(157, 600);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
+            this.optAdministrar_usuarios.AutoSize = false;
+            this.optAdministrar_usuarios.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.optAdministrar_usuarios.Image = global::Usuarios.Properties.Resources.images__2_;
+            this.optAdministrar_usuarios.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.optAdministrar_usuarios.Name = "optAdministrar_usuarios";
+            this.optAdministrar_usuarios.Size = new System.Drawing.Size(64, 64);
+            this.optAdministrar_usuarios.Text = "toolStripButton1";
             // 
             // toolStripLabel1
             // 
             this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(154, 20);
+            this.toolStripLabel1.Size = new System.Drawing.Size(121, 15);
             this.toolStripLabel1.Text = "Administrar Usuarios";
+            // 
+            // optAgregar_Herramientas
+            // 
+            this.optAgregar_Herramientas.AutoSize = false;
+            this.optAgregar_Herramientas.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.optAgregar_Herramientas.Image = global::Usuarios.Properties.Resources.mechanic_tool_logo_free_vector;
+            this.optAgregar_Herramientas.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.optAgregar_Herramientas.Name = "optAgregar_Herramientas";
+            this.optAgregar_Herramientas.Size = new System.Drawing.Size(64, 64);
+            this.optAgregar_Herramientas.Text = "toolStripButton2";
             // 
             // toolStripLabel2
             // 
             this.toolStripLabel2.Name = "toolStripLabel2";
-            this.toolStripLabel2.Size = new System.Drawing.Size(154, 20);
+            this.toolStripLabel2.Size = new System.Drawing.Size(121, 15);
             this.toolStripLabel2.Text = "Agregar Herramientas";
+            // 
+            // optAgregar_refacciones
+            // 
+            this.optAgregar_refacciones.AutoSize = false;
+            this.optAgregar_refacciones.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.optAgregar_refacciones.Image = global::Usuarios.Properties.Resources.images__5_;
+            this.optAgregar_refacciones.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.optAgregar_refacciones.Name = "optAgregar_refacciones";
+            this.optAgregar_refacciones.Size = new System.Drawing.Size(64, 64);
+            this.optAgregar_refacciones.Text = "toolStripButton3";
             // 
             // toolStripLabel3
             // 
             this.toolStripLabel3.Name = "toolStripLabel3";
-            this.toolStripLabel3.Size = new System.Drawing.Size(154, 20);
+            this.toolStripLabel3.Size = new System.Drawing.Size(121, 15);
             this.toolStripLabel3.Text = "Agregar Refacciones";
+            // 
+            // optBuscar_herramientas
+            // 
+            this.optBuscar_herramientas.AutoSize = false;
+            this.optBuscar_herramientas.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.optBuscar_herramientas.Image = global::Usuarios.Properties.Resources.descargas__4_;
+            this.optBuscar_herramientas.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.optBuscar_herramientas.Name = "optBuscar_herramientas";
+            this.optBuscar_herramientas.Size = new System.Drawing.Size(64, 64);
+            this.optBuscar_herramientas.Text = "toolStripButton4";
             // 
             // toolStripLabel4
             // 
             this.toolStripLabel4.Name = "toolStripLabel4";
-            this.toolStripLabel4.Size = new System.Drawing.Size(154, 20);
+            this.toolStripLabel4.Size = new System.Drawing.Size(121, 15);
             this.toolStripLabel4.Text = "Buscar Herramientas";
+            // 
+            // optBuscar_refacciones
+            // 
+            this.optBuscar_refacciones.AutoSize = false;
+            this.optBuscar_refacciones.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.optBuscar_refacciones.Image = global::Usuarios.Properties.Resources.images__6_;
+            this.optBuscar_refacciones.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.optBuscar_refacciones.Name = "optBuscar_refacciones";
+            this.optBuscar_refacciones.Size = new System.Drawing.Size(64, 64);
+            this.optBuscar_refacciones.Text = "toolStripButton5";
             // 
             // toolStripLabel5
             // 
             this.toolStripLabel5.Name = "toolStripLabel5";
-            this.toolStripLabel5.Size = new System.Drawing.Size(154, 20);
+            this.toolStripLabel5.Size = new System.Drawing.Size(121, 15);
             this.toolStripLabel5.Text = "Buscar Refacciones";
+            // 
+            // optBuscar_usuarios
+            // 
+            this.optBuscar_usuarios.AutoSize = false;
+            this.optBuscar_usuarios.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.optBuscar_usuarios.Image = global::Usuarios.Properties.Resources.images__7_;
+            this.optBuscar_usuarios.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.optBuscar_usuarios.Name = "optBuscar_usuarios";
+            this.optBuscar_usuarios.Size = new System.Drawing.Size(64, 64);
+            this.optBuscar_usuarios.Text = "toolStripButton6";
             // 
             // toolStripLabel6
             // 
             this.toolStripLabel6.Name = "toolStripLabel6";
-            this.toolStripLabel6.Size = new System.Drawing.Size(154, 20);
+            this.toolStripLabel6.Size = new System.Drawing.Size(121, 15);
             this.toolStripLabel6.Text = "Buscar Usuarios";
             // 
-            // toolStripButton1
+            // optSalir
             // 
-            this.toolStripButton1.AutoSize = false;
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = global::Usuarios.Properties.Resources.images__2_;
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(64, 64);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.optSalir.AutoSize = false;
+            this.optSalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.optSalir.Image = ((System.Drawing.Image)(resources.GetObject("optSalir.Image")));
+            this.optSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.optSalir.Name = "optSalir";
+            this.optSalir.Size = new System.Drawing.Size(64, 64);
+            this.optSalir.Text = "toolStripButton1";
             // 
-            // toolStripButton2
+            // toolStripLabel7
             // 
-            this.toolStripButton2.AutoSize = false;
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = global::Usuarios.Properties.Resources.mechanic_tool_logo_free_vector;
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(64, 64);
-            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripLabel7.Name = "toolStripLabel7";
+            this.toolStripLabel7.Size = new System.Drawing.Size(121, 15);
+            this.toolStripLabel7.Text = "Salir";
             // 
-            // toolStripButton3
+            // toolstrip
             // 
-            this.toolStripButton3.AutoSize = false;
-            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton3.Image = global::Usuarios.Properties.Resources.images__5_;
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(64, 64);
-            this.toolStripButton3.Text = "toolStripButton3";
-            // 
-            // toolStripButton4
-            // 
-            this.toolStripButton4.AutoSize = false;
-            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton4.Image = global::Usuarios.Properties.Resources.descargas__4_;
-            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(64, 64);
-            this.toolStripButton4.Text = "toolStripButton4";
-            // 
-            // toolStripButton5
-            // 
-            this.toolStripButton5.AutoSize = false;
-            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton5.Image = global::Usuarios.Properties.Resources.images__6_;
-            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton5.Name = "toolStripButton5";
-            this.toolStripButton5.Size = new System.Drawing.Size(64, 64);
-            this.toolStripButton5.Text = "toolStripButton5";
-            // 
-            // toolStripButton6
-            // 
-            this.toolStripButton6.AutoSize = false;
-            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton6.Image = global::Usuarios.Properties.Resources.images__7_;
-            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton6.Name = "toolStripButton6";
-            this.toolStripButton6.Size = new System.Drawing.Size(64, 64);
-            this.toolStripButton6.Text = "toolStripButton6";
+            this.toolstrip.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.toolstrip.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolstrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolstrip.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.toolstrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optAdministrar_usuarios,
+            this.toolStripLabel1,
+            this.optAgregar_Herramientas,
+            this.toolStripLabel2,
+            this.optAgregar_refacciones,
+            this.toolStripLabel3,
+            this.optBuscar_herramientas,
+            this.toolStripLabel4,
+            this.optBuscar_refacciones,
+            this.toolStripLabel5,
+            this.optBuscar_usuarios,
+            this.toolStripLabel6,
+            this.optSalir,
+            this.toolStripLabel7});
+            this.toolstrip.Location = new System.Drawing.Point(0, 0);
+            this.toolstrip.Name = "toolstrip";
+            this.toolstrip.Size = new System.Drawing.Size(124, 696);
+            this.toolstrip.TabIndex = 0;
+            this.toolstrip.Text = "toolStrip1";
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1068, 600);
-            this.Controls.Add(this.toolStrip1);
+            this.ClientSize = new System.Drawing.Size(1019, 696);
+            this.Controls.Add(this.toolstrip);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Menu";
             this.Text = "Menu";
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
+            this.toolstrip.ResumeLayout(false);
+            this.toolstrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,18 +206,20 @@
 
         #endregion
 
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton optAdministrar_usuarios;
         private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton optAgregar_Herramientas;
         private System.Windows.Forms.ToolStripLabel toolStripLabel2;
-        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton optAgregar_refacciones;
         private System.Windows.Forms.ToolStripLabel toolStripLabel3;
-        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton optBuscar_herramientas;
         private System.Windows.Forms.ToolStripLabel toolStripLabel4;
-        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton optBuscar_refacciones;
         private System.Windows.Forms.ToolStripLabel toolStripLabel5;
-        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.ToolStripButton optBuscar_usuarios;
         private System.Windows.Forms.ToolStripLabel toolStripLabel6;
+        private System.Windows.Forms.ToolStripButton optSalir;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel7;
+        private System.Windows.Forms.ToolStrip toolstrip;
     }
 }
