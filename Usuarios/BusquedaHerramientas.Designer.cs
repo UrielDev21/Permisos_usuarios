21374af843108c954ab7a0abdf2e3d056cb35edd
Uriel <luevanosaavedra@gmail.com>
2024-10-09 20:22:42 -0600

    Commit

--- Usuarios/BusquedaHerramientas.Designer.cs
+++ Usuarios/BusquedaHerramientas.Designer.cs
@@ -28,79 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtgvTaller = new System.Windows.Forms.DataGridView();
             this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvTaller)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtBuscar
             // 
-            this.textBox1.Location = new System.Drawing.Point(153, 112);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(875, 62);
-            this.textBox1.TabIndex = 29;
+            this.txtBuscar.Location = new System.Drawing.Point(91, 96);
+            this.txtBuscar.Multiline = true;
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(848, 25);
+            this.txtBuscar.TabIndex = 29;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 129);
+            this.label1.Location = new System.Drawing.Point(11, 96);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.Size = new System.Drawing.Size(74, 20);
             this.label1.TabIndex = 30;
             this.label1.Text = "Busqueda:";
             // 
-            // dataGridView1
+            // dtgvTaller
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 197);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1039, 391);
-            this.dataGridView1.TabIndex = 31;
+            this.dtgvTaller.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvTaller.Location = new System.Drawing.Point(16, 143);
+            this.dtgvTaller.Name = "dtgvTaller";
+            this.dtgvTaller.RowHeadersWidth = 51;
+            this.dtgvTaller.RowTemplate.Height = 24;
+            this.dtgvTaller.Size = new System.Drawing.Size(924, 436);
+            this.dtgvTaller.TabIndex = 31;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(394, 35);
+            this.label5.Location = new System.Drawing.Point(18, 12);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(370, 36);
+            this.label5.Size = new System.Drawing.Size(302, 29);
             this.label5.TabIndex = 32;
             this.label5.Text = "Busqueda de herramientas";
             // 
-            // button1
+            // btnCerrar
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(1022, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(40, 36);
-            this.button1.TabIndex = 33;
-            this.button1.Text = "X";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCerrar.ForeColor = System.Drawing.Color.White;
+            this.btnCerrar.Location = new System.Drawing.Point(910, 12);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(36, 36);
+            this.btnCerrar.TabIndex = 33;
+            this.btnCerrar.Text = "X";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.btnCerrar);
+            this.panel1.Location = new System.Drawing.Point(-7, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(958, 57);
+            this.panel1.TabIndex = 34;
             // 
             // BusquedaHerramientas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1068, 600);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(949, 600);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dtgvTaller);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Controls.Add(this.txtBuscar);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "BusquedaHerramientas";
             this.Text = "BusquedaHerramientas";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvTaller)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,10 +121,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtBuscar;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dtgvTaller;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Usuarios/BusquedaHerramientas.cs
+++ Usuarios/BusquedaHerramientas.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using manej
 namespace Usuarios
 {
     public partial class BusquedaHerramientas : Form
     {
+        
+
         public BusquedaHerramientas()
         {
             InitializeComponent();
